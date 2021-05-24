5ed514d545c7eb026ea76f0286f2d77c831ead34
Eldar Kalachev <rusgunter@gmail.com>
2021-05-24 18:16:00 +0300

    spark groupBy, functions: avg, round, alias

---
+++ spark-sql/friends-by-age-with-dataframe.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession, Row, functions as func
+
+
+spark = SparkSession.builder.appName("FriendsByAge").getOrCreate()
+
+friends = (spark.read
+           .option("header", "true")
+           .option("inferSchema", "true")
+           .csv("../data/fakefriends-header.csv"))
+
+friendsByAge = friends.select("age", "friends").groupBy("age")
+
+# Sorted
+friendsByAge.avg("friends").sort("age").show()
+
+# Formated more nicely
+friendsByAge.agg(func.round(func.avg("friends"), 2)).sort("age").show()
+
+# With a custom column name
+friendsByAge.agg(func.round(func.avg("friends"), 2)
+                 .alias("friends_avg")).sort("age").show()
+
+spark.stop()
